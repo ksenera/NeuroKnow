96ec353548e6db97aaee4bf7a5ff0633a420d799
ksenera <ksenera7130@conestogac.on.ca>
2025-10-27 22:37:26 -0400

    new class using OOP

---
+++ neuroknow.py
@@ -0,0 +1,8 @@
+# neuroknow.py
+
+from dataclasses import dataclass
+from typing import List, Dict, Optional, Tuple
+from enum import Enum
+import numpy as np
+from datetime import datetime
+import json
